44587a4a88134f25f03331ec72b54b965075a7fb
h-1-y <h-1-y@naver.com>
2024-05-09 23:08:45 +0900

    회원가입 폼 유효성 검사 1차 추가

--- src/main/java/h1y/site/jpa/controller/AdminLoginController.java
+++ src/main/java/h1y/site/jpa/controller/AdminLoginController.java
@@ -1,14 +1,17 @@
 package h1y.site.jpa.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import h1y.site.jpa.domain.Address;
-import h1y.site.jpa.domain.Admin;
 import h1y.site.jpa.form.AdminForm;
 import h1y.site.jpa.service.AdminService;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +19,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdminLoginController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	private final AdminService adminService;
 	
 	@GetMapping("adm/login")
@@ -28,16 +33,33 @@ public class AdminLoginController {
 	@GetMapping("adm/join")
 	public String adminJoin(Model model) {
 		
+		model.addAttribute("adminForm", new AdminForm());
+		
 		return "admin/login/join";
 		
 	}
 	
 	@PostMapping("adm/join")
-	public String adminJoinProc(@Valid AdminForm adminForm) {
+	public String adminJoinProc(@Valid AdminForm adminForm, BindingResult result) {
+		
+		// validation check
+		if ( result.hasErrors() ) {
+			
+			System.out.println("check here");
+			
+			for ( FieldError error : result.getFieldErrors() ) {
+				
+				logger.info("■ JOIN VALIDATION ERROR > : " + error.getDefaultMessage());
+				
+			}
+			
+			return "admin/login/join";
+			
+		}
 		
 		adminService.join(adminForm);
 		
-		return "redirect:/";
+		return "redirect:/adm/login";
 		
 	}
 	
--- src/main/java/h1y/site/jpa/form/AdminForm.java
+++ src/main/java/h1y/site/jpa/form/AdminForm.java
@@ -1,19 +1,39 @@
 package h1y.site.jpa.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AdminForm {
-
+	
+	@NotEmpty(message = "아이디를 입력해주세요.")
+	@Pattern(regexp = "^[a-z0-9]{3,20}$", message = "아이디는 영어 소문자와 숫자만 사용하여 3~20자리여야 합니다.")
 	private String admId;
+	
+	@NotEmpty(message = "비밀번호를 입력해주세요.")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$", message = "비밀번호는 8~16자리수여야 합니다. 영문 소문자, 숫자, 특수문자를 1개 이상 포함해야 합니다.")
 	private String admPassword;
+	
+	@NotEmpty(message = "비밀번호 확인을 입력해주세요.")
 	private String admPasswordCheck;
+	
+	@NotEmpty(message = "이름를 입력해주세요.")
 	private String admName;
+	
+	@NotEmpty(message = "이메일을 입력해주세요.")
+	@Email
 	private String email;
+	
+	@NotEmpty(message = "주소를 입력해주세요.")
 	private String address;
+	
 	private String adrsDet;
+	
+	@NotEmpty(message = "우편번호를 입력해주세요.")
 	private String zipcode;
 	
 }
